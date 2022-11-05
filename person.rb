1093d4f2008cf419e56bced57815d545c3932698
Lena <summyalena@gmail.com>
2022-11-05 04:31:25 +0100

    linters corrections

--- person.rb
+++ person.rb
@@ -1,22 +1,22 @@
-
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
-    def initialize(name = "unknown", age, parent_permission: true)
-        @id = Random.rand(1...1000) 
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
+  def initialize(name = 'unknown', age, parent_permission: true)
+      @id = Random.rand(1...1000)
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+  end
 
-    private
+  def can_use_services?
+      is_of_age? || @parent_permission
+  end
 
-    def is_of_age?
-        @age >= 18
-        end
+  private
+
+  def is_of_age?
+      @age >= 18
+  end
 
 end
