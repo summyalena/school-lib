d5f17e35f5a54dd53531fdae3c13645be02468ba
Lena <summyalena@gmail.com>
2022-11-05 04:15:56 +0100

    correcting linters

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
+
 class Person
     attr_reader :id
     attr_accessor :name, :age
-    def initialize(name = "unknown", age, parent_permission = true)
+    def initialize(name = "unknown", age, parent_permission: true)
         @id = Random.rand(1...1000) 
         @name = name
         @age = age
